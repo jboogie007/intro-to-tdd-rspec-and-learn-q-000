f2be114ac2c20a1a7f9a086f9e58ba108e8f4a02
Jason Pitts <sandwichshop3578@outlook.com>
2016-05-21 23:19:13 -0400

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,6 @@
+def current_age_for_birth_year(current_age_for_birth_year)
+ current_year = 2016
+ age_of_person = current_year - current_age_for_birth_year
+
+end
+ current_age_for_birth_year(1984)
